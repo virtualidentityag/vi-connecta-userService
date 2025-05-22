4219e67521b9497c2c3ec328f6915ee3a3e23e00
tkuzynow <tomasz.kuzynowski@gmail.com>
2025-05-22 16:18:21 +0200

    fix: enable global security config problem

--- src/main/java/de/caritas/cob/userservice/api/config/auth/SecurityConfig.java
+++ src/main/java/de/caritas/cob/userservice/api/config/auth/SecurityConfig.java
@@ -16,7 +16,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
